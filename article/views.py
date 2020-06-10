055c1f5567b225651758f608474c80c08dc60c04
Ebrahim <30graph@gmail.com>
2020-06-11 00:22:45 +0430

    update

--- article/views.py
+++ article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from . import models
 
 # Create your views here.
@@ -6,3 +6,6 @@ def article_list(request):
     article=models.Article.objects.all().order_by('date')
     args = {'article':article} 
     return render(request,'article/art_list.html',args) 
+def article_detail(request,slug):
+    return HttpResponse(slug)
+
